5c4ec33b4695fdc862225c177fccd632d9125318
lgelbman <lgelbman@student.touro.edu>
2021-02-21 16:51:47 -0500

    completed linked list and added timer in main for ints and strings

--- main.cpp
+++ main.cpp
@@ -1,44 +1,103 @@
 #include <iostream>
 #include "MyVector.h"
 #include "LLIterator.h"
+#include <string>
+
+#include <chrono>
+using std::chrono::system_clock;
+using std::chrono::steady_clock;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::milliseconds;
 
 using namespace std;
 
+void vectAddAndRemoveIntegers();
+void linkedListAddAndRemoveIntegers();
+template <typename Func>long
+long TimeFunc(Func f);
+void vectAddAndRemoveStrings();
+void linkedListAddAndRemoveStrings();
+string getRandomString();
+
+
+const int numOfOperations = 10000;
+
 int main()
 {
+    cout << "test" << endl;
+    long vectorTimeForIntegers = TimeFunc(vectAddAndRemoveIntegers);
+    long linkedListTimeForIntegers = TimeFunc(linkedListAddAndRemoveIntegers);
+    long vectorTimeForStrings = TimeFunc(vectAddAndRemoveStrings);
+    long linkedListTimeForStrings = TimeFunc(linkedListAddAndRemoveStrings);
+    cout << vectorTimeForIntegers << endl;
+    cout << linkedListTimeForIntegers << endl;
+    cout << vectorTimeForStrings << endl;
+    cout << linkedListTimeForStrings << endl;
     
-    MyVector<int> vect;
-    for (int i = 0; i < 20; i++) {
-        vect.add(i * i);
-        cout << i * i << endl;
-    }
-
-    cout << "_______" << endl;
-
-    for (int i = 0; i < 20; i++) {
-        cout << vect.get(i) << endl;
-    }
-
-    cout << "----------" << endl;
-
-    LLIterator<int> list;
-    for (int i = 0; i < 20; i++) {
-        list.add(i * i);
-    }
-
-    while (list.hasNext()){
-        int num = list.getNext();
-        cout << num << endl;
-    }
-
-    
-
-
-    
-
-
     return 0;
 }
 
+//copied from "Timing Example.txt"
+template <typename Func>long
+long TimeFunc(Func f){
+    auto begin = steady_clock::now();
+    f();
+    auto end = steady_clock::now();
+    return duration_cast<milliseconds>(end - begin).count();
+}
 
+void vectAddAndRemoveIntegers() {
+    MyVector<int> vector;
+    for (int i = 0; i < numOfOperations; i++) {
+        vector.add(rand());
+    }
+    for (int i = 0; i < numOfOperations; i++) {
+        vector.get(i);
+    }
+}
 
+void linkedListAddAndRemoveIntegers() {
+    LLIterator<int> linkedList;
+    for (int i = 0; i < numOfOperations; i++) {
+        linkedList.add(i);
+    }
+    for (int i = 0; i < numOfOperations; i++) {
+        linkedList.getNext();
+    }
+}
+
+void vectAddAndRemoveStrings() {
+    MyVector<string> vector;
+    for (int i = 0; i < numOfOperations; i++) {
+        string s1 = getRandomString();
+        vector.add(s1);
+    }
+    for (int i = 0; i < numOfOperations; i++) {
+        vector.get(i);
+    }
+}
+
+void linkedListAddAndRemoveStrings() {
+    LLIterator<string> linkedList;
+    for (int i = 0; i < numOfOperations; i++) {
+        string s1 = getRandomString();
+        linkedList.add(s1);
+    }
+    for (int i = 0; i < numOfOperations; i++) {
+        linkedList.getNext();
+    }
+}
+
+string getRandomString() {
+    char alphabet[] = {'a', 'b', 'c','d','e','f',
+        'g','h','i','j','k','l','m','n','o','p','q',
+        's','t','u','v','w','x','y','z'};
+    int len = rand() % 200;
+    string str1;
+    for (int i = 0; i < len; i++) {
+        char randomChar = rand() % 26;
+        str1 += alphabet[randomChar];
+    }
+    return str1;
+}
